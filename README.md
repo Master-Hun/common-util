bf1ba2232957add56abf4a7a25b2695993196fbc
Hun <hun@Huns-MacBook-Pro.local>
2021-11-23 05:49:12 +0900

    jwt 헬퍼

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# @master-hun/common-util
+# @masterh8887/duksan-common-util
 
 :chart_with_upwards_trend: common-module
 
@@ -11,13 +11,13 @@
 ### 설치
 
 ```bash
-npm i -S @master-hun/common-util
+npm i -S @masterh8887/duksan-common-util
 ```
 
 ### JWT 기본 사용법
 
 ```typescript
-import { JwtHelper, ManageRole, ServiceRole } from "@master-hun/common-util";
+import { JwtHelper } from "@masterh8887/duksan-common-util";
 
 const helper = new JwtHelper();
 const jwtObject = helper.getJwtObject(accessToken);
@@ -29,10 +29,10 @@ const jwtObject = helper.getJwtObject(accessToken);
 jwtObject.isExpired();
 ```
 
-* 회원 번호
+* 관리자/고객 번호
 
 ```typescript
-jwtObject.getUserId();
+jwtObject.getUserSeq();
 ```
 
 * 접속 IP 주소
@@ -49,16 +49,6 @@ jwtObject.getUserType();
 UserType.SYSTEM;    // 사용자 유형 (시스템) : react에서 미사용
 UserType.MASTER    // 사용자 유형 (최고 관리자) : 최고 관리자 권한
 UserType.MANAGE     // 사용자 유형 (관리자) : 관리자 권한
-
-```
-
-* 권한 보유 여부 확인 방법
-
-```typescript
-// 관리자 권한 사용자정보 읽기권한 보유 여부
-jwtObject.hasReadRole(ManageRole.USER_INFO);
-
-// 관리자 권한 사용자정보 쓰기권한 보유 여부
-jwtObject.hasWriteRole(ManageRole.USER_INFO);
+UserType.CUSTOMER   // 사용자 유형 (고객) : 고객 권한
 
 ```
--- src/jwt.object.ts
+++ src/jwt.object.ts
@@ -1,14 +1,15 @@
 /**
  * 사용자 유형.
- * - SYSTEM : 시스템 (실제 로그인 하는 사용자가 아님)
- * - MANAGE : 관리자 (서비스 관리를 하는 사용자)
- * - SERVICE : 서비스 (실제 서비스를 사용하는 사용자)
- * - PARTNER : 파트너 (파트너 사용자, 현재 미사용, 향후 확장을 위해 정의)
+ * - SYSTEM : 시스템 (시스템)
+ * - MASTER : 서비스 (최고 관리자)
+ * - MANAGE : 관리자 (서비스 관리자)
+ * - CUSTOMER : 고객 (서비스 이용자)
  */
 export enum UserType {
     SYSTEM = "system",
     MASTER = "master",
     MANAGE = "manage",
+    CUSTOMER = "customer",
 };
 
 /** JWT Header. */
@@ -21,26 +22,18 @@ export interface JwtHeader {
  * JWT Payload.
  * - iss : 
  * - exp : 토큰 만료시간 (unix time 포맷)
- * - uid : 사용자번호 (number)
+ * - useq : 관리자/고객번호 (number)
  * - ip : 접속 IP 주소
  * - urs : 사용자 권한 (bigint)
- * - brs (optional) : 이용가능한 사업번호 배열 (number array with comma seperated)
- *   - SERVICE 사용자 유형인 경우에만 해당
- *   - 관리자 사용자가 SERVICE 사용자 권한을 임시 발급 받는 경우에도 해당함
  * - ut (optional) : 시용자 유형, 생략시 SYSTEM
- * - fid (optional) : 시설번호
- *   - SERVICE 사용자 유형인 경우에만 해당
- *   - 관리자 사용자가 SERVICE 사용자 권한을 임시 발급 받는 경우에도 해당함
  */
 export interface JwtPayload {
     iss: string;
     exp: string;
-    uid: string;
+    useq: string;
     ip: string;
     urs: string;
-    brs?: string;
     ut?: string;
-    fid?: string;
 };
 
 export class JwtObject {
@@ -76,11 +69,11 @@ export class JwtObject {
     }
 
     /**
-     * 회원번호를 반환한다.
-     * @returns 회원번호 (숫자)
+     * 관리자/고객 번호를 반환한다.
+     * @returns 관리자/고객번호 (숫자)
      */
-    public getUserId(): number {
-        return Number(this.payload.uid);
+    public getUserSeq(): number {
+        return Number(this.payload.useq);
     }
 
     /**
@@ -107,10 +100,12 @@ export class JwtObject {
         switch (this.payload.ut) {
             case "system":
                 return UserType.SYSTEM;
-            case "manage":
-                return UserType.MANAGE;
             case "master":
                 return UserType.MASTER;
+            case "manage":
+                return UserType.MANAGE;
+            case "customer":
+                return UserType.CUSTOMER;
             default:
                 return UserType.SYSTEM;
         }
